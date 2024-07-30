a8b5dbce5fff85a3ec1d34ab5e85704ebc776ee5
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-07-30 22:09:28 +0300

    Resolve conflict

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
-import NavBar from "../components/NavBar"
+import NavBar from "../components/NavBar";
 import Hero from "../components/sections/Hero";
 
 const HomePage = () => {
-  return (
-    <>
-      <div className="bg-slate-900">
-        <NavBar/>
-        <Hero />
-      </div>
-    </>
-  )
-}
+    return (
+        <>
+            <div className="bg-slate-900">
+                <Hero />
+                <NavBar />
+            </div>
+        </>
+    );
+};
 
 export default HomePage;
